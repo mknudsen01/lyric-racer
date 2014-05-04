6cd41921adfb9a6d93c0763af823787232f2a790
Matthew Knudsen <mknudsen01@gmail.com>
2014-05-04 15:41:00 -0700

    Add functionality to determine the pressed key
      * Correctly deals with shift vs no shift
      * Ignores the shift button itself

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ Racer.Binder = function(config){
 
 Racer.Binder.prototype = {
   keyDownListener: function(){
-    $(document).on('keydown', this.game.keyPressed);
+    $(document).on('keydown', this.game.keyPressed.bind(this.game));
   }
 };
 
@@ -20,7 +20,71 @@ Racer.Game.prototype = {
   },
 
   keyPressed: function(event){
-    console.log(event.which);
+    var pressedCharacter = this.getCharacter(event);
+  },
+
+  getCharacter: function(event){
+    var _to_ascii = {
+      '186': '59',
+      '187': '61',
+      '188': '44',
+      '189': '45',
+      '190': '46',
+      '191': '47',
+      '192': '96',
+      '220': '92',
+      '222': '39',
+      '221': '93',
+      '219': '91',
+      '173': '45',
+    };
+
+    var shiftUps = {
+      "16": "shift",
+      "32": " ",
+      "96": "~",
+      "49": "!",
+      "50": "@",
+      "51": "#",
+      "52": "$",
+      "53": "%",
+      "54": "^",
+      "55": "&",
+      "56": "*",
+      "57": "(",
+      "48": ")",
+      "45": "_",
+      "61": "+",
+      "91": "{",
+      "93": "}",
+      "92": "|",
+      "59": ":",
+      "39": "\"",
+      "44": "<",
+      "46": ">",
+      "47": "?"
+    };
+
+
+    event.preventDefault();
+
+    var characterCode = event.which;
+    var character = '';
+    if(_to_ascii.hasOwnProperty(characterCode)){
+      characterCode = _to_ascii[characterCode];
+    }
+
+    if(!event.shiftKey && (characterCode>=65 && characterCode<=90)){
+      character = String.fromCharCode(characterCode + 32);
+    } else if(event.shiftKey && shiftUps.hasOwnProperty(characterCode)) {
+      character = shiftUps[characterCode];
+    } else {
+      character = String.fromCharCode(characterCode);
+    }
+
+    if(character != "shift"){
+      return character;
+    }
   }
 };
 
